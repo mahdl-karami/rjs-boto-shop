14aaca96e96a40897cc133c942803e47eb94cc66
mohammad mahdi <mo.mahdi.ka@gmail.com>
2024-01-31 11:23:12 +0330

    Cart Empty Added

--- src/Pages/Cart.jsx
+++ src/Pages/Cart.jsx
@@ -8,21 +8,22 @@ import ProductPanel from "../Components/ProductPanel";
 import CartPanel from "../Components/CartPanel";
 
 const Cart = () => {
-	const state = useSelector((state) => state.Cart);
-	return (
-		<div className={styles.cart}>
-      <CartPanel  state={state}/>
-			<div className={styles.products}>
-				{state.products.map((p, i) => (
-					<div key={i} className={styles.productItem}>
-						<img src={p.image} alt={ShortThis(p.title, 3)} />
-						<h5>{ShortThis(p.title, 3)}</h5>
-						<ProductPanel product={p} />
-					</div>
-				))}
-			</div>
-		</div>
-	);
+  const state = useSelector((state) => state.Cart);
+  return (
+    <div className={styles.cart}>
+      <CartPanel state={state} />
+      <div className={styles.products}>
+        {!state.products.length && <h1>Cart Is Empty ...</h1>}
+        {state.products.map((p, i) => (
+          <div key={i} className={styles.productItem}>
+            <img src={p.image} alt={ShortThis(p.title, 3)} />
+            <h5>{ShortThis(p.title, 3)}</h5>
+            <ProductPanel product={p} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Cart;
